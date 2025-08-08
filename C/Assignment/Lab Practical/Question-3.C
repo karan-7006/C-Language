a8210364ccd2a8cb71674c8cafd27754e8512f6f
karan-7006 <kharvarkaran21@gmail.com>
2025-08-08 21:03:53 +0530

    file name

---
+++ C/Assignment/Lab Practical/Question-3.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	clrscr(); //Clear Screen
+
+	//Variable declarations
+	int age=20;
+	char grade='a';
+	float height=5.8;
+
+	//Displaying Values
+	printf("Age: %d\n", age);
+	printf("Grade: %c\n", grade);
+	printf("Height: %.1f\n", height);
+
+	getch(); //Holds the Result
+}
+
